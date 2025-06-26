50eb11c1c380903b390a39126d1ace2656195b5e
Bhavani2112 <bhavani.chnn@gmail.com>
2025-06-26 15:42:09 -0500

    Added extent.flush()

--- mmppixel/src/main/java/org/iitwf/lib/FrameworkLibrary.java
+++ mmppixel/src/main/java/org/iitwf/lib/FrameworkLibrary.java
@@ -8,6 +8,7 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 
@@ -83,6 +84,14 @@ public class FrameworkLibrary {
 		return prop;
 
 	}
+	@AfterSuite
+   	public void tearDownExtentReport() {
+        if (extent != null) {
+            extent.flush();
+            System.out.println("Extent report flushed and saved.");
+        }
+    }
+
 
 
 }
